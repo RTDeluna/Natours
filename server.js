006e57b46b54e7601a55b1970f082248aaf4579b
RTDeluna <ramueldeluna30@gmail.com>
2023-01-12 16:20:40 +0800

    before MVC

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose'); //1 
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace( //1
+const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-mongoose //1
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,12 +18,44 @@ mongoose //1
   .then((con) => {
     console.log(con.connections);
     console.log('DB connections successful!');
-  }).catch((err)=>{  // use this code to catch database error
-    console.log(err)
-  })
-  
+  });
+// .catch((err) => {
+//   // use this code to catch database error
+//   console.log(err);
+// });
 
 // console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 🔥 ', err);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
