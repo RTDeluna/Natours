6109cf6df6299df2196435d7cf29207e0ff861a9
RTDeluna <ramueldeluna30@gmail.com>
2023-01-12 14:39:58 +0800

    DB connect added

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose'); //1 
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace( //1
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose //1
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connections successful!');
+  }).catch((err)=>{  // use this code to catch database error
+    console.log(err)
+  })
+  
 
 // console.log(process.env);
 
