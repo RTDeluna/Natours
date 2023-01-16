218846b6f043e397bee011709f5d2b480506cb5f
RTDeluna <ramueldeluna30@gmail.com>
2023-01-16 13:24:12 +0800

    After validation

--- Routes/tourRoutes.js
+++ Routes/tourRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const tourController = require('./../Controllers/tourController');
 
 const router = express.Router();
-
 // router.param('id', tourController.checkID);
 
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
